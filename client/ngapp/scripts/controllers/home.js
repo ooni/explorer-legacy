f43febe9e021cc87c0ac2dc349ad039064d99adc
Arturo Filastò <art@fuffa.org>
2015-05-31 20:33:18 +0200

    Implement listing with flags

--- client/ngapp/scripts/controllers/home.js
+++ client/ngapp/scripts/controllers/home.js
@@ -7,11 +7,31 @@
  * # HomeCtrl
  * Controller of the ooniAPIApp
  */
+
 angular.module('ooniAPIApp')
   .controller('HomeCtrl', function ($scope, Report) {
-    Report.listReportFiles({
+    $scope.reports_by_country = [];
+
+    var data = Report.listReportFiles({
       by: "probe_cc"
-    }, function(data, headers) {
-      $scope.report_files = data;
+    }, function(res, headers) {
+        angular.forEach(data, function(reports, cc) {
+          var report_files = [];
+          // XXX ghetto hax
+          if (cc[0] == "$")
+            return;
+          angular.forEach(reports, function(report) {
+            report_files.push({
+              report_filename: report.report_filename,
+              date: report.report_filename !== undefined ? report.report_filename.split("-")[0] : "unknown"
+            });
+          });
+
+          $scope.reports_by_country.push({
+            country: ISOCountryNames[cc],
+            country_code: cc,
+            report_files: report_files
+          })
+      });
     });
 });
