18d6a539210016b861be990958bb51dc359d5e65
Simon <svansintjan@gmail.com>
2015-12-09 12:03:00 -0800

    Design pretty much up to snuff with mockups #3

--- client/ngapp/scripts/controllers/explore.js
+++ client/ngapp/scripts/controllers/explore.js
@@ -14,4 +14,56 @@ angular.module('ooniAPIApp')
     Report.findReports({}, function(response) {
       $scope.reports = response.reports;
     });
-});
+})
+
+/**
+ * @ngdoc function
+ * @name ooniAPIApp.directive:ooniReportsTable
+ * @description
+ * # ooniReportsTable
+ * A directive that wraps rows of the ooni reports
+ */
+
+.directive('ooniReportsTable', ["$location", "$filter", "Report", "Country", "uiGridConstants",
+  function ($location, $filter, Report, Country, uiGridConstants) {
+    return {
+      restrict: 'A',
+      scope: {
+        reports: '=',
+        showCountry: '='
+      },
+      link: function ($scope, $element, $attrs) {
+
+      },
+      templateUrl: 'views/directives/reports-table.html',
+    };
+}])
+
+/**
+ * @ngdoc function
+ * @name ooniAPIApp.directive:ooniReportsTableRow
+ * @description
+ * # ooniReportsTableRow
+ * A directive that displays each OONI report as a table row.
+ */
+
+.directive('ooniReportsTableRow', ["$location", "$filter", "Report", "Country", "uiGridConstants",
+  function ($location, $filter, Report, Country, uiGridConstants) {
+    return {
+      restrict: 'A',
+      scope: {
+        report: '=',
+        showCountry: '='
+      },
+      link: function ($scope, $element, $attrs) {
+        $scope.viewReport = function(report) {
+          $location.path('/report/' + report.id);
+        };
+
+        $scope.viewCountry = function(countryCode) {
+          $location.path('/country/' + countryCode);
+        };
+      },
+      templateUrl: 'views/directives/reports-table-row.html',
+    };
+}]);
