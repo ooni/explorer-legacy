0051c62b276f054d4156a10b51398b2b7cb6e136
Arturo Filastò <art@fuffa.org>
2015-12-24 20:54:37 +0100

    Also copy over ttf and woff

--- Gruntfile.js
+++ Gruntfile.js
@@ -342,7 +342,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/*'
+            'styles/fonts/*',
+            'styles/*.{woff,ttf}'
           ]
         }, {
           expand: true,
