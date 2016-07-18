b8988fc7d8d631b11cbe50582ba65186f960e7b0
Arturo Filastò <arturo@filasto.net>
2016-07-18 23:59:27 +0200

    Add some better exception handling

--- server/config.local.js
+++ server/config.local.js
@@ -10,5 +10,20 @@ module.exports = {
   indexFile: require.resolve(isDevEnv ?
     '../client/ngapp/index.html' : '../client/dist/index.html'),
   port: GLOBAL_CONFIG.port,
-  legacyExplorer: GLOBAL_CONFIG.legacyExplorer
+  legacyExplorer: GLOBAL_CONFIG.legacyExplorer,
+  remoting: {
+    errorHandler: {
+      handler: function(error, req, res, next) {
+        if (error instanceof Error) {
+          console.log(
+            'Error in %s %s: errorName=%s errorMessage=%s \n errorStack=%s',
+              req.method, req.url, error.name, error.message, error.stack);
+        }
+        else {
+          console.log(req.method, req.originalUrl, res.statusCode, error);
+        }
+        next();
+      }
+    }
+  }
 };
