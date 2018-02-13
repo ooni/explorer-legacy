7877cdff6f647b766a547177bb2983d0dbff1c03
Arturo Filastò <arturo@filasto.net>
2018-02-13 12:25:27 +0200

    Enable order_by

--- common/models/report.js
+++ common/models/report.js
@@ -26,12 +26,11 @@ module.exports = function(Report) {
       apiQuery.test_name = test_name
     }
 
-    /*
     if (order) {
       apiQuery.order_by = order.split(' ')[0]
       apiQuery.order = order.split(' ')[1]
     }
-    */
+
     if (page_number && page_size) {
       apiQuery.offset = page_number * page_size
       apiQuery.limit = page_size
